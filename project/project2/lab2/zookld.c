4cf9318dad6cd2497d9df89e8183d8c50ccc3094
Deepak Gangwar <deeepakg1998@gmail.com>
2018-02-13 17:10:41 +0530

    ex2 complete ex3 in progress

--- project/project2/lab2/zookld.c
+++ project/project2/lab2/zookld.c
@@ -144,16 +144,26 @@ pid_t launch_svc(CONF *conf, const char *name)
                     break;
     }
 
+    if ((dir = NCONF_get_string(conf, name, "dir")))
+    {
+        /* chroot into dir */
+        if(chdir(dir)) err(1,"error in changing directory before chroot");
+        else chroot(dir);
+        if(chroot(".")) err(1,"error in chroot");
+    }
+
     if (NCONF_get_number_e(conf, name, "uid", &uid))
     {
         /* change real, effective, and saved uid to uid */
         warnx("setuid %ld", uid);
+        setresuid(uid,uid,uid);
     }
 
     if (NCONF_get_number_e(conf, name, "gid", &gid))
     {
         /* change real, effective, and saved gid to gid */
         warnx("setgid %ld", gid);
+        setresgid(gid,gid,gid);
     }
 
     if ((groups = NCONF_get_string(conf, name, "extra_gids")))
@@ -163,12 +173,9 @@ pid_t launch_svc(CONF *conf, const char *name)
         /* set the grouplist to gids */
         for (i = 0; i < ngids; i++)
             warnx("extra gid %d", gids[i]);
+        setgroups(ngids,gids);
     }
 
-    if ((dir = NCONF_get_string(conf, name, "dir")))
-    {
-        /* chroot into dir */
-    }
 
     signal(SIGCHLD, SIG_DFL);
     signal(SIGPIPE, SIG_DFL);
